5f4de3e85f76aed669d803feb6d07eb6f27839e6
Beast13 <betteliamlfm@gmail.com>
2018-12-10 11:12:56 +0100

    Bla bla

---
+++ app/src/main/java/com/liam/ihmapp/InscriptionPart1Activity.java
@@ -0,0 +1,13 @@
+package com.liam.ihmapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class InscriptionPart1Activity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inscription_part1);
+    }
+}
--- app/src/main/java/com/liam/ihmapp/MainActivity.java
+++ app/src/main/java/com/liam/ihmapp/MainActivity.java
@@ -1,13 +1,31 @@
 package com.liam.ihmapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+
+    public void LogIn(View view) {
+    }
+
+    /**
+     * Launch the Register Form Part 1 for a new user.
+     * @param view
+     */
+    public void RegisterForm(View view) {
+        Intent intent = new Intent(this, InscriptionPart1Activity.class);
+        startActivity(intent);
     }
 }
