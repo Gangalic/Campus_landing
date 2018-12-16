e2587ed04d78e9994d597488a2087bb862515098
Catalin <gangalicatalin@gmail.com>
2018-12-16 15:01:32 +0100

    Profil horizontal switch fix

--- app/src/main/java/com/liam/ihmapp/ProfileActivity.java
+++ app/src/main/java/com/liam/ihmapp/ProfileActivity.java
@@ -2,8 +2,6 @@ package com.liam.ihmapp;
 
 import android.content.Intent;
 import android.support.design.widget.TabLayout;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 
@@ -85,41 +83,6 @@ public class ProfileActivity extends AppCompatActivity {
     }
 
     /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        public PlaceholderFragment() {
-        }
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                                 Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_profile2, container, false);
-            TextView textView = (TextView) rootView.findViewById(R.id.section_label);
-            textView.setText(getString(R.string.section_format, getArguments().getInt(ARG_SECTION_NUMBER)));
-            return rootView;
-        }
-    }
-
-    /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
      */
@@ -131,15 +94,34 @@ public class ProfileActivity extends AppCompatActivity {
 
         @Override
         public Fragment getItem(int position) {
-            // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+            // Return the current tabs
+            switch(position) {
+                case 0:
+                    Tab1Profil tab1 = new Tab1Profil();
+                    return tab1;
+                case 1:
+                    Tab2Parametres tab2 = new Tab2Parametres();
+                    return tab2;
+                default:
+                    return null;
+            }
         }
 
         @Override
         public int getCount() {
             // Show 2 total pages.
             return 2;
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position){
+            switch(position) {
+                case 0:
+                    return "Profil";
+                case 1:
+                    return "Parametres";
+            }
+            return null;
         }
     }
 }
