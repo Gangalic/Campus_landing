b8dbd5bcb4e044c4ede82692cf0f6203776064cf
LBeast13 <liam.bette@hotmail.com>
2018-12-26 12:30:07 +0100

    Some changes in Want a Thing Activity

--- app/src/main/java/com/liam/ihmapp/WantAThing.java
+++ app/src/main/java/com/liam/ihmapp/WantAThing.java
@@ -1,44 +1,32 @@
 package com.liam.ihmapp;
 
 import android.content.Intent;
+import android.support.v4.app.DialogFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.text.Editable;
 import android.text.TextWatcher;
 import android.view.View;
 import android.widget.EditText;
+import android.widget.TextView;
 
 import static java.lang.String.valueOf;
 
 public class WantAThing extends AppCompatActivity {
 
-    private EditText category;
+
     private EditText element;
-    private EditText startDate;
     private EditText period;
+    private TextView startDate;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_want_athing);
 
-        category = findViewById(R.id.editText9);
         element = findViewById(R.id.editText8);
-        startDate = findViewById(R.id.editText5);
+        startDate = findViewById(R.id.date_emprunt);
         period = findViewById(R.id.editText7);
-
-        category.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void onTextChanged(CharSequence s, int start, int before, int count) {
-                category.setBackgroundColor(getResources().getColor(R.color.colorWhite));
-            }
-
-            // unused but declared
-            @Override
-            public void beforeTextChanged(CharSequence s, int start, int count, int after) {}
-            @Override
-            public void afterTextChanged(Editable s) {}
-        });
 
         element.addTextChangedListener(new TextWatcher() {
             @Override
@@ -82,15 +70,12 @@ public class WantAThing extends AppCompatActivity {
 
     private boolean checkFields(){
         String el = valueOf(element.getText().toString());
-        String cat = valueOf(category.getText().toString());
         String sDate = valueOf(startDate.getText().toString());
         String per = valueOf(period.getText().toString());
 
-        if (el.length() == 0 || cat.length() == 0 || sDate.length() == 0 || per.length() == 0){
+        if (el.length() == 0 || sDate.length() == 0 || per.length() == 0){
             if(el.length() == 0)
                 element.setBackgroundColor(getResources().getColor(R.color.colorAccent));
-            if(cat.length() == 0)
-                category.setBackgroundColor(getResources().getColor(R.color.colorAccent));
             if(sDate.length() == 0)
                 startDate.setBackgroundColor(getResources().getColor(R.color.colorAccent));
             if(per.length() == 0)
@@ -117,7 +102,22 @@ public class WantAThing extends AppCompatActivity {
             startActivity(intent);
         }
     }
+
+    public void processDatePickerResult(int year, int month, int day) {
+        String month_string = Integer.toString(month+1);
+        String day_string = Integer.toString(day);
+        String year_string = Integer.toString(year);
+        String dateMessage = (day_string + "/" + month_string + "/" + year_string);
+
+        startDate.setText(dateMessage);
+    }
+
+    public void pickADate(View view) {
+        DialogFragment newFragment = new DatePickerFragment();
+        newFragment.show(getSupportFragmentManager(),"Sélectionnez une date");
+    }
 }
+
 
 
 
