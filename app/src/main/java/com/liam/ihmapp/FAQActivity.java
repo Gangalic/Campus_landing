6c87fc1de3c93f2d9f9d863fe383f8c7759955df
LBeast13 <liam.bette@hotmail.com>
2018-12-27 16:05:38 +0100

    FAQ Activity improved

--- app/src/main/java/com/liam/ihmapp/FAQActivity.java
+++ app/src/main/java/com/liam/ihmapp/FAQActivity.java
@@ -19,13 +19,23 @@ public class FAQActivity extends AppCompatActivity {
 
     private LinearLayout faq1Layout;
     private ImageView faq1Image;
-    private TextView faq1Title;
+    private TextView faq1Answer;
     private int faq1Clicked = 0;
 
     private LinearLayout faq2Layout;
     private ImageView faq2Image;
-    private TextView faq2Title;
+    private TextView faq2Answer;
     private int faq2Clicked = 0;
+
+    private LinearLayout faq3Layout;
+    private ImageView faq3Image;
+    private TextView faq3Answer;
+    private int faq3Clicked = 0;
+
+    private LinearLayout faq4Layout;
+    private ImageView faq4Image;
+    private TextView faq4Answer;
+    private int faq4Clicked = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,21 +44,20 @@ public class FAQActivity extends AppCompatActivity {
 
         faq1Layout = (LinearLayout) findViewById(R.id.faq1_layout);
         faq1Image = (ImageView) findViewById(R.id.faq_img_1);
-        faq1Title = (TextView) findViewById(R.id.faq1_title);
-
+        faq1Answer = (TextView) findViewById(R.id.faq_answer1);
 
         faq2Layout = (LinearLayout) findViewById(R.id.faq2_layout);
         faq2Image = (ImageView) findViewById(R.id.faq_img_2);
-        faq2Title = (TextView) findViewById(R.id.faq2_title);
+        faq2Answer = (TextView) findViewById(R.id.faq_answer2);
 
+        faq3Layout = (LinearLayout) findViewById(R.id.faq3_layout);
+        faq3Image = (ImageView) findViewById(R.id.faq_img_3);
+        faq3Answer = (TextView) findViewById(R.id.faq3_answer);
 
-    }
+        faq4Layout = (LinearLayout) findViewById(R.id.faq4_layout);
+        faq4Image = (ImageView) findViewById(R.id.faq_img_4);
+        faq4Answer = (TextView) findViewById(R.id.faq4_answer);
 
-    @Override
-    public void onPostCreate(@Nullable Bundle savedInstanceState, @Nullable PersistableBundle persistentState) {
-        super.onPostCreate(savedInstanceState, persistentState);
-        initFAQLayout(faq1Layout,faq1Title);
-        initFAQLayout(faq2Layout,faq2Title);
     }
 
     public void launchProfile(View view) {
@@ -74,33 +83,41 @@ public class FAQActivity extends AppCompatActivity {
     //-------FAQ Click events ---------------------
 
     public void expandFAQ1(View view) {
-        faq1Clicked = faqExpandHide(faq1Layout,faq1Image, faq1Title, faq1Clicked);
+        faq1Clicked = faqExpandHide(faq1Layout,faq1Image, faq1Answer, faq1Clicked);
     }
 
     public void expandFAQ2(View view) {
-        faq2Clicked = faqExpandHide(faq2Layout,faq2Image, faq2Title, faq2Clicked);
+        faq2Clicked = faqExpandHide(faq2Layout,faq2Image, faq2Answer, faq2Clicked);
+    }
+
+    public void expandFAQ3(View view) {
+        faq3Clicked = faqExpandHide(faq3Layout,faq3Image, faq3Answer, faq3Clicked);
+    }
+
+    public void expandFAQ4(View view) {
+        faq4Clicked = faqExpandHide(faq4Layout,faq4Image, faq4Answer, faq4Clicked);
     }
 
     /**
      *
      * @param lay the layout containing the current FAQ
      * @param img the image button of the FAQ
-     * @param title the title of the current FAQ
+     * @param answer the answer of the current FAQ
      * @param clicked if the FAQ was already clicked
      * @return the new state of the FAQ
      */
-    public int faqExpandHide(LinearLayout lay, ImageView img, TextView title, int clicked){
+    public int faqExpandHide(LinearLayout lay, ImageView img, TextView answer, int clicked){
         switch(clicked) {
             case 0:
                 lay.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
-                lay.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
-                img.setImageResource(R.drawable.ic_expand_24dp);
+                answer.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT));
+                img.setImageResource(R.drawable.ic_expand_white_24dp);
                 clicked = 1;
                 break;
             case 1:
                 lay.getLayoutTransition().enableTransitionType(LayoutTransition.CHANGING);
-                initFAQLayout(lay,title);
-                img.setImageResource(R.drawable.ic_faq_hidden_24dp);
+                answer.setLayoutParams(new LinearLayout.LayoutParams(LinearLayout.LayoutParams.MATCH_PARENT, 0));
+                img.setImageResource(R.drawable.ic_hide_white_24dp);
                 clicked = 0;
                 break;
         }
@@ -108,10 +125,6 @@ public class FAQActivity extends AppCompatActivity {
         return clicked;
     }
 
-    public void initFAQLayout(LinearLayout lay,TextView title){
-        LinearLayout.LayoutParams params = (LinearLayout.LayoutParams) lay.getLayoutParams();
-        params.height = title.getHeight() + 2*lay.getPaddingTop();
-        lay.setLayoutParams(params);
-    }
+
 
 }
